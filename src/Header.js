150273cd64faedfc9cc837077797b75ef1d49b0e
BrakeLJ <lukejbrake@gmail.com>
2020-10-01 18:07:24 -0400

    header component

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <>
+      <header>
+          <h1>Save the Lyric!</h1>
+      </header>
+    </>
+  )
+}
+
+export default Header;
