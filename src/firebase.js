309fb49e4a27b6659b9e0519dafda3997464aaee
RyanTjong88 <ryan.tjong888@gmail.com>
2020-10-01 16:48:53 -0400

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+ // Your web app's Firebase configuration
+    const firebaseConfig = {
+        apiKey: "AIzaSyA3-ydP6B_gCEZnb3Qhw_bO8Rb58aMUutA",
+        authDomain: "savethelyric.firebaseapp.com",
+        databaseURL: "https://savethelyric.firebaseio.com",
+        projectId: "savethelyric",
+        storageBucket: "savethelyric.appspot.com",
+        messagingSenderId: "90336307330",
+        appId: "1:90336307330:web:40ef96b904978aafcbec9b"
+    };
+  // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
