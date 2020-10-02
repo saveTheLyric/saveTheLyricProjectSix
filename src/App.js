49f957de7105822f6c11b65ed87e94da95273994
Sarah <sarahmichelleduong@gmail.com>
2020-10-01 20:49:42 -0400

    rough draft layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Header from './Header';
+import Header from './Header.js';
+import Body from "./Body.js"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Body/>
       </div>
     );
   }
