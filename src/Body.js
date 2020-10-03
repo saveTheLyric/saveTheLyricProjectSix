4d408b72be29a20cad8fb7fbc93f2ed1d573bf93
RyanTjong88 <ryan.tjong888@gmail.com>
2020-10-03 03:53:22 -0400

    stored data into firebase and rendered back to DOM

--- src/Body.js
+++ src/Body.js
@@ -1,9 +1,11 @@
-import Axios from "axios";
 import React, { Component } from "react";
+import Axios from "axios";
+import firebase from './firebase';   
+
 
 class Body extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       firebaseData: {
         artist: "",
@@ -36,10 +38,48 @@ class Body extends Component {
   getLyrics = (e) => {
     e.preventDefault();
     Axios.get(`https://api.lyrics.ovh/v1/${this.state.firebaseData.artist}/${this.state.firebaseData.song}`)
-    .then((results) => {
-      
-      console.log(results);
+    .then((response) => {
+      console.log(response.data.lyrics);
+      const lyrics = response.data.lyrics
+      this.setState({
+        firebaseData: {
+          artist: this.state.firebaseData.artist,
+          song: this.state.firebaseData.song,
+          lyrics
+        }
+      });
     })
+  }
+
+updatedLyrics = () => {
+// reset input field
+  this.setState({
+    firebaseData: {
+      artist: "",
+      song: "",
+      lyrics: ""
+    }  
+  })
+}
+
+  // Store firebaseData: (artist, song, lyrics) to firebase
+  firebase = (event) => {
+    event.preventDefault();
+  
+    // open portal to Firebase
+    const dbRef = firebase.database().ref()
+
+    // add new record to Firebase
+    dbRef.push(this.state.firebaseData);
+
+    // reset input field
+    this.setState({
+      firebaseData: {
+        artist: "",
+        song: "",
+        lyrics: ""
+      }    
+    });
   }
 
   render(){
@@ -70,13 +110,50 @@ class Body extends Component {
               <button type="submit">Submit</button>
             </form>
           </div>
-          <div className="lyrics">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, adipisci omnis ipsum assumenda magni veniam, fuga, rerum quae incidunt voluptate sit aspernatur ipsa optio earum recusandae ducimus quas nisi quod ab. Facilis quo debitis assumenda amet, nisi optio perferendis fuga. Nesciunt quo eligendi iusto eaque est aut minus recusandae optio.</p>
-          </div>
+
+          {/* button to push to firebase */}
+          {/* option 1 */}
+          {/* <section className="right">
+            <div className="lyrics">
+              <p value={this.state.firebaseData.lyrics}>
+                {this.state.firebaseData.lyrics}
+              </p>
+            </div>
+            <div className="buttonContainer">
+              <button className="saveLyrics" onClick={this.firebase}>
+                Store lyrics
+              </button>
+            </div>
+          </section> */}
+
+          {/* option 2 */}
+          <section className="right">
+            <div className="lyrics">
+                <form action="submit" onSubmit={this.firebase}>
+
+                  <textarea 
+                  name="plans" 
+                  cols="30" 
+                  rows="10" 
+                  minLength="10" 
+                  maxLength=""
+                  onChange={this.updatedLyrics}
+                  value={this.state.firebaseData.lyrics}
+                  >
+                    {this.state.firebaseData.lyrics}
+                  </textarea>
+
+                  <div className="buttonContainer">
+                    <button className="saveLyrics">Store lyrics</button>
+                  </div>
+
+              </form>
+            </div>
+          </section>
         </div>
       // </div>
 
-    )
+    );
   }
 }
 
